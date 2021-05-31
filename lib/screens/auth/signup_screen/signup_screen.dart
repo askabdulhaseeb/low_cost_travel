26af55c0dc79ef77c6db8084eff979e2401fd86d
askabdulhaseeb <askabdulhaseeb@gmail.com>
2021-05-31 13:47:45 +0500

    add basic folders and files and set the project

---
+++ lib/screens/auth/signup_screen/signup_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatelessWidget {
+  static const routeName = '/SignupScreen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Signup Screen'),
+      ),
+    );
+  }
+}
---
+++ lib/screens/map_screen/map_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MapScreen extends StatelessWidget {
+  static const routeName = '/MapScreen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Map Screen'),
+      ),
+    );
+  }
+}
